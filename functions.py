5206522e5d4ef6ada5faa0c09da4e85255426c7d
Vytautas <vytautas@bunevicius.com>
2024-05-29 15:04:57 +0300

    Visualization improvements

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from statsmodels.regression.linear_model import OLS
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union, Optional
 
 import statsmodels.api as sm
 import plotly.express as px
@@ -14,9 +14,10 @@ import plotly.graph_objects as go
 import plotly.subplots as sp
 import plotly.io as pio
 
-PRIMARY_COLORS = ["#5684F7", "#3A5CED", "#7E7AE6"]
-SECONDARY_COLORS = ["#7BC0FF", "#B8CCF4", "#18407F", "#85A2FF", "#C2A9FF", "#3D3270"]
+PRIMARY_COLORS = ["#5684F7", "#3A5CED", "#7E7AE6", "#C2A9FF"]
+SECONDARY_COLORS = ["#7BC0FF", "#B8CCF4", "#18407F", "#85A2FF", "#3D3270"]
 ALL_COLORS = PRIMARY_COLORS + SECONDARY_COLORS
+
 
 def get_columns(df: pd.DataFrame) -> List[str]:
     """
@@ -48,7 +49,13 @@ def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def plot_box_chart(df: pd.DataFrame, x_label: str, y_label: str, chart_title: str, save_path: str = None) -> None:
+def plot_box_chart(
+    df: pd.DataFrame,
+    x_label: str,
+    y_label: str,
+    chart_title: str,
+    save_path: str = None,
+) -> None:
     """
     Creates a box plot for each column in the DataFrame and optionally saves it as an image.
 
@@ -66,19 +73,31 @@ def plot_box_chart(df: pd.DataFrame, x_label: str, y_label: str, chart_title: st
     plot_box_chart(df, "X Label", "Y Label", "Box Chart Title", save_path="plot.png")
     """
     fig = go.Figure()
-    for col in df.columns:
+    for i, col in enumerate(df.columns):
         fig.add_trace(
             go.Box(
                 y=df[col],
                 name=col,
                 boxpoints="outliers",
-                marker_color=SECONDARY_COLORS[4],
+                marker_color=PRIMARY_COLORS[i % len(PRIMARY_COLORS)],
                 line_color=SECONDARY_COLORS[0],
             )
         )
     fig.update_layout(
-        title=chart_title, xaxis_title=x_label, yaxis_title=y_label, title_x=0.5
+        title=chart_title,
+        xaxis_title=x_label,
+        yaxis_title=y_label,
+        title_x=0.5,
+        title_font=dict(size=20),
+        showlegend=False,
+        template="plotly_white",
+        height=500,
+        width=1600,
+        margin=dict(l=50, r=50, t=80, b=50),  # Adjusted margins for consistency
     )
+    fig.update_xaxes(title_font=dict(size=14))
+    fig.update_yaxes(title_font=dict(size=14))
+
     fig.show()
 
     if save_path:
@@ -103,29 +122,31 @@ def identify_outliers(df: pd.DataFrame) -> Dict[str, Union[pd.Series, int]]:
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
 
-    outliers = ((df < lower_bound) | (df > upper_bound))
+    outliers = (df < lower_bound) | (df > upper_bound)
     outliers_count = outliers.sum()
 
     total_outliers = outliers.values.sum()
 
-    return {
-        'outliers_per_column': outliers_count,
-        'total_outliers': total_outliers
-    }
+    return {"outliers_per_column": outliers_count, "total_outliers": total_outliers}
 
 
-def plot_histograms(df: pd.DataFrame, features: List[str], nbins: int = 40, save_path: str = None) -> None:
+def plot_histograms(
+    df: pd.DataFrame, features: List[str], nbins: int = 40, save_path: str = None
+) -> None:
     """
-    Plots histograms for specified features in the DataFrame.
+    Plots histograms for specified features in the DataFrame with the feature names only on the x-axis.
 
     Parameters:
     - df (pd.DataFrame): DataFrame to plot.
     - features (List[str]): List of features to plot histograms for.
     - nbins (int): Number of bins to use in histograms.
+    - save_path (str): Path to save the image file (optional).
     """
     title = f"Distribution of {', '.join(features)}"
+    rows = 1
+    cols = len(features)
 
-    fig = sp.make_subplots(rows=1, cols=len(features))
+    fig = sp.make_subplots(rows=rows, cols=cols, horizontal_spacing=0.1)
 
     for i, feature in enumerate(features):
         fig.add_trace(
@@ -134,16 +155,27 @@ def plot_histograms(df: pd.DataFrame, features: List[str], nbins: int = 40, save
                 nbinsx=nbins,
                 name=feature,
                 marker=dict(
-                    color=ALL_COLORS[i % len(ALL_COLORS)],
+                    color=PRIMARY_COLORS[
+                        i % len(PRIMARY_COLORS)
+                    ],  # Ensure PRIMARY_COLORS is defined or adjust as necessary
                     line=dict(color="#000000", width=1),
                 ),
             ),
             row=1,
             col=i + 1,
         )
+        fig.update_xaxes(title_text=feature, row=1, col=i + 1, title_font=dict(size=14))
+        fig.update_yaxes(title_text="Count", row=1, col=i + 1, title_font=dict(size=14))
 
     fig.update_layout(
-        title_text=title, title_x=0.5, showlegend=False
+        title_text=title,
+        title_x=0.5,
+        title_font=dict(size=20),
+        showlegend=False,
+        template="plotly_white",
+        height=500,
+        width=400 * cols,
+        margin=dict(l=50, r=50, t=80, b=50),  # Adjusted top margin for the main title
     )
 
     fig.show()
@@ -152,7 +184,7 @@ def plot_histograms(df: pd.DataFrame, features: List[str], nbins: int = 40, save
         fig.write_image(save_path)
 
 
-def plot_heatmap(corr_matrix: pd.DataFrame, save_path: str = None) -> None:
+def plot_heatmap(corr_matrix: pd.DataFrame, save_path: Optional[str] = None) -> None:
     """
     Plots a heatmap based on a correlation matrix using a specified color scheme.
 
@@ -164,11 +196,11 @@ def plot_heatmap(corr_matrix: pd.DataFrame, save_path: str = None) -> None:
     - None
     """
     colorscale = [
-        [0.0, SECONDARY_COLORS[5]], 
-        [0.2, SECONDARY_COLORS[4]],  
-        [0.4, PRIMARY_COLORS[2]],  
-        [0.6, PRIMARY_COLORS[1]],  
-        [0.8, PRIMARY_COLORS[0]],  
+        [0.0, SECONDARY_COLORS[4]],
+        [0.2, SECONDARY_COLORS[3]],
+        [0.4, PRIMARY_COLORS[2]],
+        [0.6, PRIMARY_COLORS[1]],
+        [0.8, PRIMARY_COLORS[0]],
         [1.0, SECONDARY_COLORS[0]],
     ]
 
@@ -178,21 +210,48 @@ def plot_heatmap(corr_matrix: pd.DataFrame, save_path: str = None) -> None:
             x=corr_matrix.columns,
             y=corr_matrix.columns,
             colorscale=colorscale,
+            colorbar=dict(
+                title="Correlation",
+                titleside="right",
+                titlefont=dict(size=14, family="Arial, sans-serif"),
+                tickfont=dict(size=12, family="Arial, sans-serif"),
+            ),
         )
     )
 
     fig.update_layout(
-        title="Correlation Matrix",
-        xaxis_title="Features",
-        yaxis_title="Features",
-        title_x=0.5,
+        title=dict(
+            text="Correlation Matrix",
+            x=0.5,
+            xanchor="center",
+            font=dict(size=20, family="Arial, sans-serif"),
+        ),
+        xaxis=dict(
+            title="Features",
+            titlefont=dict(size=14, family="Arial, sans-serif"),
+            tickfont=dict(size=12, family="Arial, sans-serif"),
+            tickangle=45,
+        ),
+        yaxis=dict(
+            title="Features",
+            titlefont=dict(size=14, family="Arial, sans-serif"),
+            tickfont=dict(size=12, family="Arial, sans-serif"),
+        ),
+        template="plotly_white",
+        height=500,
+        width=1600,
+        margin=dict(l=100, r=100, t=100, b=100),
     )
+
     fig.show()
+
     if save_path:
         fig.write_image(save_path)
 
 
-def test_correlation(data: pd.DataFrame, var1: str, var2: str) -> Dict[str, Union[float, bool, Tuple[float, float]]]:
+def test_correlation(
+    data: pd.DataFrame, var1: str, var2: str
+) -> Dict[str, Union[float, bool, Tuple[float, float]]]:
     """
     Calculate the Pearson correlation between two variables and test the null hypothesis that the correlation is zero.
 
@@ -226,7 +285,9 @@ def test_correlation(data: pd.DataFrame, var1: str, var2: str) -> Dict[str, Unio
     }
 
 
-def plot_coefficients(model: OLS, title: str = "Coefficient Estimates and Confidence Intervals") -> None:
+def plot_coefficients(
+    model: OLS, title: str = "Coefficient Estimates and Confidence Intervals"
+) -> None:
     """
     Creates a bar plot of model coefficients and their confidence intervals using predefined color codes.
 
@@ -239,16 +300,24 @@ def plot_coefficients(model: OLS, title: str = "Coefficient Estimates and Confid
     """
     coefficients = model.params
     conf_int = model.conf_int()
-    conf_int_df = pd.DataFrame(conf_int, columns=['Lower CI', 'Upper CI'])
-    coefficients_df = pd.DataFrame(coefficients, columns=['Coefficient'])
+    conf_int_df = pd.DataFrame(conf_int, columns=["Lower CI", "Upper CI"])
+    coefficients_df = pd.DataFrame(coefficients, columns=["Coefficient"])
     coefficients_summary = coefficients_df.join(conf_int_df)
 
-    fig = px.bar(coefficients_summary, y='Coefficient', error_y='Upper CI', error_y_minus='Lower CI', title=title,
-                 color_discrete_sequence=[PRIMARY_COLORS[1]])
+    fig = px.bar(
+        coefficients_summary,
+        y="Coefficient",
+        error_y="Upper CI",
+        error_y_minus="Lower CI",
+        title=title,
+        color_discrete_sequence=[PRIMARY_COLORS[1]],
+    )
     fig.show()
 
 
-def plot_correlation(data: pd.DataFrame, var1: str, var2: str, save_path: str = None) -> go.Figure:
+def plot_correlation(
+    data: pd.DataFrame, var1: str, var2: str, save_path: Optional[str] = None
+) -> go.Figure:
     """
     Visualize the Pearson correlation between two variables using Plotly, with improved annotation placement and specified trendline color.
 
@@ -256,15 +325,22 @@ def plot_correlation(data: pd.DataFrame, var1: str, var2: str, save_path: str = 
     - data (pd.DataFrame): The dataset containing the variables.
     - var1 (str): The name of the first variable.
     - var2 (str): The name of the second variable.
-    - save_path (str): The file path to save the plot as a PNG image.
+    - save_path (str, optional): The file path to save the plot as a PNG image.
 
     Returns:
     - go.Figure: A Plotly figure object with the scatter plot, regression line, and improved annotations.
     """
     stats = test_correlation(data, var1, var2)
 
-    fig = px.scatter(data, x=var1, y=var2, trendline="ols", labels={var1: var1, var2: var2},
-                     title=f"Scatter Plot of {var1} vs {var2}", color_discrete_sequence=[SECONDARY_COLORS[0]])
+    fig = px.scatter(
+        data,
+        x=var1,
+        y=var2,
+        trendline="ols",
+        labels={var1: var1, var2: var2},
+        title=f"Scatter Plot of {var1} vs {var2}",
+        color_discrete_sequence=[SECONDARY_COLORS[0]],
+    )
 
     results = px.get_trendline_results(fig)
     slope = results.iloc[0]["px_fit_results"].params[1]
@@ -272,34 +348,52 @@ def plot_correlation(data: pd.DataFrame, var1: str, var2: str, save_path: str = 
 
     fig.data[-1].line.color = SECONDARY_COLORS[4]
 
-    fig.add_trace(go.Scatter(
-        x=[None],
-        y=[None],
-        text=[f"Correlation: {stats['Correlation']:.3f}<br>P-value: {stats['P-Value']:.3g}<br>"
-              f"95% CI: [{stats['95% CI'][0]:.3f}, {stats['95% CI'][1]:.3f}]<br>"
-              f"Line Equation: y = {slope:.3f}x + {intercept:.3f}"],
-        mode="text",
-        textposition="bottom right",
-        showlegend=False,
-        textfont=dict(family="sans serif", size=12, color="black")
-    ))
+    fig.add_trace(
+        go.Scatter(
+            x=[None],
+            y=[None],
+            text=[
+                f"Correlation: {stats['Correlation']:.3f}<br>P-value: {stats['P-Value']:.3g}<br>"
+                f"95% CI: [{stats['95% CI'][0]:.3f}, {stats['95% CI'][1]:.3f}]<br>"
+                f"Line Equation: y = {slope:.3f}x + {intercept:.3f}"
+            ],
+            mode="text",
+            textposition="bottom right",
+            showlegend=False,
+            textfont=dict(family="sans serif", size=12, color="black"),
+        )
+    )
 
     fig.update_layout(
-        xaxis=dict(domain=[0, 0.85]),
-        title_x=0.5,
-        annotations=[{
-            'text': f"Statistical Details:<br>Correlation: {stats['Correlation']:.3f}<br>P-value: {stats['P-Value']:.3g}<br>"
-                    f"95% CI: [{stats['95% CI'][0]:.3f}, {stats['95% CI'][1]:.3f}]<br>"
-                    f"Line Equation: y = {slope:.3f}x + {intercept:.3f}",
-            'align': 'left',
-            'showarrow': False,
-            'xref': 'paper',
-            'yref': 'paper',
-            'x': 1.05,
-            'y': 0.5,
-            'bordercolor': 'black',
-            'borderwidth': 1
-        }]
+        title={
+            "text": f"Scatter Plot of {var1} vs {var2}",
+            "x": 0.5,
+            "xanchor": "center",
+            "font": {"size": 20},
+        },
+        xaxis_title=var1,
+        yaxis_title=var2,
+        xaxis_title_font=dict(size=14),
+        yaxis_title_font=dict(size=14),
+        template="plotly_white",
+        margin=dict(l=50, r=50, t=80, b=50),  # Adjusted margins for consistency
+        annotations=[
+            {
+                "text": f"Statistical Details:<br>Correlation: {stats['Correlation']:.3f}<br>P-value: {stats['P-Value']:.3g}<br>"
+                f"95% CI: [{stats['95% CI'][0]:.3f}, {stats['95% CI'][1]:.3f}]<br>"
+                f"Line Equation: y = {slope:.3f}x + {intercept:.3f}",
+                "align": "left",
+                "showarrow": False,
+                "xref": "paper",
+                "yref": "paper",
+                "x": 1.05,
+                "y": 0.5,
+                "bordercolor": "black",
+                "borderwidth": 1,
+            }
+        ],
+        height=500,
+        width=1600,
     )
 
     fig.show()
@@ -330,7 +424,9 @@ def log_transform_features(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame
     return transformed_df
 
 
-def train_linear_model(data: pd.DataFrame, target_column: str) -> Tuple[OLS, pd.DataFrame, pd.Series, np.ndarray]:
+def train_linear_model(
+    data: pd.DataFrame, target_column: str
+) -> Tuple[OLS, pd.DataFrame, pd.Series, np.ndarray]:
     """
     Trains a linear regression model using the given data and target column.
 
@@ -350,7 +446,9 @@ def train_linear_model(data: pd.DataFrame, target_column: str) -> Tuple[OLS, pd.
 
     X = sm.add_constant(X)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     model = OLS(y_train, X_train)
     results = model.fit()
@@ -359,7 +457,7 @@ def train_linear_model(data: pd.DataFrame, target_column: str) -> Tuple[OLS, pd.
 
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
-    adj_r2 = 1 - (1-r2)*(len(y)-1)/(len(y)-X.shape[1]-1)
+    adj_r2 = 1 - (1 - r2) * (len(y) - 1) / (len(y) - X.shape[1] - 1)
 
     print(f"Model summary:\n{results.summary()}")
     print(f"Mean Squared Error: {mse}")
@@ -369,7 +467,13 @@ def train_linear_model(data: pd.DataFrame, target_column: str) -> Tuple[OLS, pd.
     return results, X_test, y_test, y_pred
 
 
-def plot_model_predictions(x_test: pd.DataFrame, y_test: pd.Series, y_pred: pd.Series, title: str, save_path: str = None) -> None:
+def plot_model_predictions(
+    x_test: pd.DataFrame,
+    y_test: pd.Series,
+    y_pred: pd.Series,
+    title: str,
+    save_path: Optional[str] = None,
+) -> None:
     """
     Plot the residuals from model predictions with a trendline, using specified color palette.
 
@@ -378,36 +482,56 @@ def plot_model_predictions(x_test: pd.DataFrame, y_test: pd.Series, y_pred: pd.S
     - y_test (pd.Series): The actual target values.
     - y_pred (pd.Series): The predicted target values.
     - title (str): The title of the plot.
-    - save_path (str): The file path to save the plot as a PNG image.
+    - save_path (str, optional): The file path to save the plot as a PNG image.
 
     Returns:
     - None
     """
     residuals = y_test - y_pred
-    results_df = pd.DataFrame({
-        'Predicted': y_pred,
-        'Residuals': residuals
-    }).reset_index(drop=True)
+    results_df = pd.DataFrame(
+        {"Predicted": y_pred, "Residuals": residuals}
+    ).reset_index(drop=True)
 
-    fig = px.scatter(results_df, x='Predicted', y='Residuals',
-                     title=title, labels={'Predicted': 'Predicted Values', 'Residuals': 'Residuals'},
-                     trendline="ols")
-    
+    fig = px.scatter(
+        results_df,
+        x="Predicted",
+        y="Residuals",
+        title=title,
+        labels={"Predicted": "Predicted Values", "Residuals": "Residuals"},
+        trendline="ols",
+    )
+
     marker_color = SECONDARY_COLORS[0]
     trendline_color = SECONDARY_COLORS[4]
 
     fig.update_traces(marker=dict(color=marker_color))
     fig.update_layout(
-        title_x=0.5,
-        plot_bgcolor='white',
-        xaxis=dict(title='Predicted Values'),
-        yaxis=dict(title='Residuals'),
-        showlegend=False
+        title={
+            "text": title,
+            "x": 0.5,
+            "xanchor": "center",
+            "font": {"size": 20, "family": "Arial, sans-serif"},
+        },
+        plot_bgcolor="white",
+        xaxis=dict(
+            title="Predicted Values",
+            titlefont=dict(size=14, family="Arial, sans-serif"),
+            tickfont=dict(size=12, family="Arial, sans-serif"),
+        ),
+        yaxis=dict(
+            title="Residuals",
+            titlefont=dict(size=14, family="Arial, sans-serif"),
+            tickfont=dict(size=12, family="Arial, sans-serif"),
+        ),
+        showlegend=False,
+        height=500,
+        width=1600,
+        margin=dict(l=50, r=50, t=80, b=50),  # Adjusted margins for consistency
     )
 
     fig.data[1].line.color = trendline_color
 
     fig.show()
-    
+
     if save_path:
         pio.write_image(fig, save_path)
